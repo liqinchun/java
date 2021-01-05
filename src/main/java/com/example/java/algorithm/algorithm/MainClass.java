7bb46f084a3909340d3055118842bbea277f6781
liqinchun <740716595@qq.com>
2021-01-06 00:55:05 +0800

    提交算法包

---
+++ src/main/java/com/example/java/algorithm/algorithm/MainClass.java
@@ -0,0 +1,15 @@
+package com.example.java.algorithm.algorithm;
+
+import org.junit.Test;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+    }
+
+    @Test
+    public  void  a1704(){
+
+    }
+}
